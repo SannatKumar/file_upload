1fac810c3daf6b3c09d6f76d8703bc0c956c5810
Raj Kumar <interplaytheheads@gmail.com>
2020-11-27 17:45:47 +0200

    res defined and typo fixed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import {useForm} from 'react-hook-form';
-import './App.css';
+
 
 
 function App() {
   const { register, handleSubmit} = useForm()
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     const formData = new FormData()
     formData.append("picture", data.picture[0])
 
     const res = await fetch("http://localhost:4000/picture", {
       method: "POST",
       body: formData
-    }).then(res.json())
+    }).then(res =>res.json())
     alert(JSON.stringify(res))
   }
   return(
@@ -23,3 +23,5 @@ function App() {
     </form>
   )
 }
+
+export default App;
